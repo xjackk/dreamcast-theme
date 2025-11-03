df0f891870b17ee50dfc92c8f1f2b19ec6bcbe43
jack <xkillilea@gmail.com>
2025-11-03 12:42:05 -0500

    update license, get rid of unused vars

--- dreamcast-theme.el
+++ dreamcast-theme.el
@@ -7,22 +7,27 @@
 ;; Version: 1.0.0
 ;; Package-Requires: ((emacs "24.1"))
 ;; Keywords: faces, themes
-;; SPDX-License-Identifier: GPL-3.0-or-later
+;; SPDX-License-Identifier: MIT
 
 ;; This file is NOT part of GNU Emacs.
 
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; Permission is hereby granted, free of charge, to any person obtaining a copy
+;; of this software and associated documentation files (the "Software"), to deal
+;; in the Software without restriction, including without limitation the rights
+;; to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+;; copies of the Software, and to permit persons to whom the Software is
+;; furnished to do so, subject to the following conditions:
 
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; The above copyright notice and this permission notice shall be included in all
+;; copies or substantial portions of the Software.
 
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+;; AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+;; SOFTWARE.
 
 ;;; Commentary:
 
@@ -61,7 +66,6 @@
 
       ;; Foreground & Text
       (fg                "#E6EDF3")  ; Crisp white-gray
-      (fg-alt            "#9CA3AF")  ; Muted gray for secondary text
       (comment           "#8B9FB5")  ; Lighter blue-gray
 
       ;; Syntax highlighting
@@ -74,8 +78,7 @@
       ;; Semantic colors
       (error             "#FF6B6B")  ; Soft red, less harsh
       (warning           "#FF8533")  ; Lighter Dreamcast orange
-      (success           "#2DD4BF")  ; Teal (blue-green) fits Dreamcast palette aesthetic
-      (info              "#00A0E9"))  ; Dreamcast electric blue!
+      (success           "#2DD4BF"))  ; Teal (blue-green) fits Dreamcast palette aesthetic
 
   (custom-theme-set-faces
    'dreamcast
